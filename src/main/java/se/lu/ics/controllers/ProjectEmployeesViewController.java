32751dcee64cda2898aa955739f4f1ef2742a85c
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-19 19:56:52 +0100

    Added support for editing Salary column

--- src/main/java/se/lu/ics/controllers/ProjectEmployeesViewController.java
+++ src/main/java/se/lu/ics/controllers/ProjectEmployeesViewController.java
@@ -12,9 +12,9 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import se.lu.ics.models.Employee;
 import se.lu.ics.models.Project;
-
 
 public class ProjectEmployeesViewController {
 
@@ -53,10 +53,17 @@ public class ProjectEmployeesViewController {
     public void initialize() {
         tableColumnEmployeeId.setCellValueFactory(new PropertyValueFactory<>("id"));
 
-        tableColumnEmployeeName.setCellFactory(TextFieldTableCell.forTableColumn());
         tableColumnEmployeeName.setCellValueFactory(new PropertyValueFactory<>("name"));
         /*
-         * This cell is editable. When the user edits the cell, the new value
+         * TextFieldTableCell is a cell that allows the user to edit the cell
+         * value. The cell value is displayed as a TextField.
+         * TextFieldTableCell can handle String values out of the box.
+         * If the cell value is not a String, a StringConverter must be
+         * specified.
+         */
+        tableColumnEmployeeName.setCellFactory(TextFieldTableCell.forTableColumn());
+        /*
+         * When the user edits the cell, the new value
          * is passed to the setOnEditCommit() method. The event object contains
          * the row value, which is the Employee object that is to be updated.
          * The new value is retrieved from the event object, and the setName()
@@ -66,15 +73,67 @@ public class ProjectEmployeesViewController {
             Employee employee = event.getRowValue();
             employee.setName(event.getNewValue());
         });
+
         tableColumnEmployeeTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        tableColumnEmployeeTitle.setCellFactory(TextFieldTableCell.forTableColumn());
         tableColumnEmployeeTitle.setOnEditCommit(event -> {
             Employee employee = event.getRowValue();
             employee.setTitle(event.getNewValue());
         });
+
         tableColumnEmployeeBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        /*
+         * TableView requires explicit instructions on how to convert
+         * the text entered by the user to a non-String type.
+         * (as the user can enter any text in the cell using their keyboard)
+         * Therefore, we need to use a custom cell factory.
+         * StringConverter is an abstract class with two abstract
+         * methods: toString() and fromString(). The toString() method
+         * that when implemented, allows us to specify how a given type
+         * should be converted to a String.
+         */
+        tableColumnEmployeeBaseSalary.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<Double>() {
+
+            /*
+             * The toString() method is called when the TableView is
+             * populated with data. The method is called for each
+             * row in the table, and the return value is displayed.
+             * %.2f means that the number should be displayed with
+             * two decimals.
+             */
+            @Override
+            public String toString(Double object) {
+                return String.format("%.2f", object);
+            }
+
+            /*
+             * The fromString() method is called when the user edits
+             * the cell. The method is called when the user presses
+             * Enter, or when the cell loses focus. The method is
+             * called with the String that the user entered in the
+             * cell. The method should return the value that the
+             * String should be converted to.
+             */
+            @Override
+            public Double fromString(String string) {
+                try {
+                    return Double.parseDouble(string);
+                } catch (NumberFormatException e) {
+                    String errorMessage = "Invalid input format. Please enter a number.";
+                    labelResponse.setText(errorMessage);
+                    return 0.0;
+                }
+            }
+        }));
+
         tableColumnEmployeeBaseSalary.setOnEditCommit(event -> {
             Employee employee = event.getRowValue();
-            employee.setBaseSalary(event.getNewValue());
+            Double newBaseSalary = event.getNewValue();
+
+            // Check if a new salary was entered
+            if (newBaseSalary != null) {
+                employee.setBaseSalary(newBaseSalary);
+            }
         });
         tableColumnEmployeeHireDate.setCellValueFactory(new PropertyValueFactory<>("hireDate"));
 
@@ -130,5 +189,5 @@ public class ProjectEmployeesViewController {
         this.project = project;
         populateTableView();
     }
-    
+
 }
