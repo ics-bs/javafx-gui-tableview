0175c1feba3048027f91101edf94b767cb600107
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-19 10:21:18 +0100

    Added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module javafx.gui.tableview {
+    exports se.lu.ics;
+    exports se.lu.ics.controllers;
+
+    opens se.lu.ics.controllers to javafx.fxml;
+    /*
+     * Required for the TableView to be able to access the Consultant class
+     * via PropertyValuesFactory
+     */
+    opens se.lu.ics.models to javafx.base;
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+}
