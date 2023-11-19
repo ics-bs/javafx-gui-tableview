9ba6ac64948832f65cd18f090f424aa28382a930
Björn Svensson <bjorn.svensson@ics.lu.se>
2023-11-19 10:21:37 +0100

    Set up couplings in App class

--- src/main/java/se/lu/ics/App.java
+++ src/main/java/se/lu/ics/App.java
@@ -1,7 +1,33 @@
 package se.lu.ics;
 
-public class App {
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import se.lu.ics.controllers.ProjectsViewController;
+import se.lu.ics.models.ProjectRegister;
+
+public class App extends Application {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ProjectRegister projectRegister = new ProjectRegister();
+
+        URL url = getClass().getResource("/fxml/ProjectsView.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        Scene scene = new Scene(loader.load());
+        ProjectsViewController controller = loader.getController();
+        controller.setProjectRegister(projectRegister);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Consultants");
+        primaryStage.show();
     }
 }
